81c01e99b0c237122b31d98dda3ba624e507f98b
Lintang <lintangwaladi@gmail.com>
2026-02-08 11:32:56 +0700

    first commit: add folder structure, add dependencies, add models: restaurant list & details

---
+++ lib/data/model/restaurant_list.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/foundation.dart';
+
+class RestaurantListResponse{
+  final bool error;
+  final String? message;
+  final int? count;
+  final int? founded;
+  final List<Restaurant> restaurants;
+
+  RestaurantListResponse({
+    required this.error,
+    this.message,
+    this.count,
+    this.founded,
+    required this.restaurants,
+  });
+
+  factory RestaurantListResponse.fromJson(Map<String,dynamic> json){
+    return RestaurantListResponse(
+      error: json['error'],
+      message: json["message"],
+      count: json["count"],
+      founded: json["founded"],
+      restaurants: List<Restaurant>.from(
+        json['restaurants'].map((r) => Restaurant.fromJson(r)),
+      ),
+    );
+  }
+}
+
+class Restaurant {
+  final String id;
+  final String name;
+  final String description;
+  final String pictureId;
+  final String city;
+  final double rating;
+
+  Restaurant({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.pictureId,
+    required this.city,
+    required this.rating,
+  });
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) {
+    return Restaurant(
+      id: json["id"],
+      name: json["name"],
+      description: json["description"],
+      pictureId: json["pictureId"],
+      city: json["city"],
+      rating: json["rating"].toDouble(),
+    );
+  }
+}
